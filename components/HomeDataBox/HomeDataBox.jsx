fc0271a48ef2f07a880138feb1a88a0b2014391c
jfrelik <j.frelik.it@outlook.com>
2021-12-05 21:27:31 +0100

    :sparkles: Add HomeDataBox.jsx
    Universal component used to hold any data on the dashboard

---
+++ components/HomeDataBox/HomeDataBox.jsx
@@ -0,0 +1,10 @@
+export default function HomeDataBox({ data, title, css }) {
+  return (
+    <div className="rounded-lg h-full flex flex-col justify-center shadow-2xl bg-white backdrop-filter backdrop-blur-md bg-opacity-70">
+      <p className={`antialiased p-3 font-black text-center text-8xl ${css}`}>
+        {data}
+      </p>
+      <p className="antialiased font-medium text-center text-xl">{title}</p>
+    </div>
+  );
+}
