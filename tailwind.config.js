08cdfaec2bf862e7cfa909f72d6ca12413e70bee
jfrelik <j.frelik.it@outlook.com>
2021-12-05 15:28:20 +0100

    :tada: Init project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  mode: 'jit',
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
